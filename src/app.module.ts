a15561c805935a5dd3e67b559e0ef68fb49a22d2
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-17 15:23:01 +0200

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { PrismaModule } from './shared/services/prisma.module';
 import { ProductModule } from './product/product.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [PrismaModule, ProductModule, UsersModule, AuthModule],
+  imports: [
+    PrismaModule,
+    ProductModule,
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
