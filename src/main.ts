25a79b953a2d4524b7f6feb4a04192f42ccdc7b3
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-17 15:39:37 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { PrismaService } from './shared/services/prisma.service';
 import { ValidationPipe } from '@nestjs/common';
 import { LoggerInterceptor } from './shared/interceptors/logger';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new LoggerInterceptor());
   app.setGlobalPrefix('api');
@@ -14,6 +16,7 @@ async function bootstrap() {
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
+  await app.listen(configService.get('port'));
   await app.listen(8000);
 }
 bootstrap();
