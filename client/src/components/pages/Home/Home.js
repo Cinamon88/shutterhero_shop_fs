cf64037a579c15990d5c154601cf156c60a640b2
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-18 13:32:18 +0200

    add Home page component

---
+++ client/src/components/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import { Row, Col } from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <div>
+            <Row className='justify-content-end align-items-center my-5'>
+                <Col className=''>
+                    <h1 className=''>Shop</h1>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default Home;
