ec43198c15010a3992584dc9b4e7a26c15a68f31
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-19 11:29:21 +0200

    change font color

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => (
-    <footer id='sticky-footer' className='py-4 mt-5 text-white-50 '>
+    <footer id='sticky-footer' className='py-4 mt-5 text-black-50 '>
       <div className='text-center'>
         <small>ShutterHero Copyright &copy; 2023</small>
       </div>
