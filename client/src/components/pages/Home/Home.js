20e8ca765cd4b5179df4a92425b1454f2a124939
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-20 12:59:31 +0200

    add Products page

--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,15 +1,11 @@
-import { Row, Col } from "react-bootstrap";
 import CarouselSlide from "../../features/Carousel/CarouselSlide";
+import Products from "../Products/Products";
 
 const Home = () => {
     return (
         <>
             <CarouselSlide />
-            <Row className='justify-content-end align-items-center my-5'>
-                <Col className=''>
-                    <h1 className=''>Shop</h1>
-                </Col>
-            </Row>
+            <Products />
         </>
     )
 }
---
+++ client/src/components/pages/Products/Products.js
@@ -0,0 +1,12 @@
+import ProductPage from "../../common/ProductsPage/Products";
+
+
+const Products = () => {
+  return (
+    <>
+      <ProductPage />
+    </>
+  );
+};
+
+export default Products;
