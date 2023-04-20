6fc79dd06a697a0b1d259159d2d9e356d3c832ca
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-20 20:25:35 +0200

    add props and modify style

--- client/src/components/common/ProductBox/ProductBox.js
+++ client/src/components/common/ProductBox/ProductBox.js
@@ -2,37 +2,43 @@ import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
 import styles from './ProductBox.module.scss';
 import { Link } from 'react-router-dom';
-import { API_URL } from '../../../config';
+import PropTypes from 'prop-types';
 
-const ProductBox = (props) => {
-  
+const ProductBox = ({
+  id,
+  name,
+  category,
+  price,
+  image,
+  oldPrice,
+}) => {
   return (
     <Card className={styles.productbox}>
-      <Card.Img variant="top" src={`./images/products/${props.image}`} />
+      <Card.Img variant="top" src={`./images/products/${image}`} />
       <Card.Body className="text-center">
         <Col className="d-flex justify-content-center">
-          {props.oldPrice > 0 ? (
+          {oldPrice > 0 ? (
             <>
-              <p className={styles.categories}>{props.category}</p>
+              <p className={styles.categories}>{category}</p>
             </>
           ) : (
-            <p className={styles.categories}> {props.category}</p>
+            <p className={styles.categories}> {category}</p>
           )}
         </Col>
-        <Card.Title className={styles.cardTitle}>{props.name}</Card.Title>
+        <Card.Title className={styles.cardTitle}>{name}</Card.Title>
         <Card.Text className="m-0">
-          {props.oldPrice > 0 ? (
+          {oldPrice > 0 ? (
             <div className={'d-flex justify-content-center ' + styles.priceDiv}>
-              <span className={styles.price}>${props.price}</span>
-              <span className={styles.oldPrice}>${props.oldPrice}</span>
+              <span className={styles.price}>${price}</span>
+              <span className={styles.oldPrice}>${oldPrice}</span>
             </div>
           ) : (
             <div className={'d-flex justify-content-center ' + styles.priceDiv}>
-              <span className={styles.normalPrice}>${props.price}</span>
+              <span className={styles.normalPrice}>${price}</span>
             </div>
           )}
         </Card.Text>
-        <Link to={`/product/` + props.id}>
+        <Link to={'/product/' + id}>
           <Button variant="outline-secondary" size="sm" className="mx-1 mb-1">
             View more
           </Button>
@@ -40,6 +46,15 @@ const ProductBox = (props) => {
       </Card.Body>
     </Card>
   );
+};
+
+ProductBox.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  oldPrice: PropTypes.number,
 };
 
 export default ProductBox;
