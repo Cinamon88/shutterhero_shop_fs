cc15073ea095d5112d82a4c8c65b50573e875c56
Lukasz Gorka <lukaszgorka@hotmail.com>
2023-04-20 22:41:10 +0200

    add orderRedux

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,11 +4,13 @@ import initialState from './initialState';
 import usersReducer from './usersRedux';
 import productsReducer from './productsRedux';
 import cartReducer from './cartRedux';
+import orderReducer from './orderRedux';
 
 const subreducers = {
   user: usersReducer,
   products: productsReducer,
   cart: cartReducer,
+  orders: orderReducer,
 }
 
 const reducer = combineReducers(subreducers);
